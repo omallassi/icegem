37956b8dd700aa5b59f2bd913febef5cba4f827c
YuliyaAidarova@gmail.com <YuliyaAidarova@gmail.com@d3097f50-16a0-dd82-721a-b39e0efbcae2>
2011-02-25 14:44:11 +0000

    enchancement of pom files and adding checking of parent classes

---
+++ trunk/icegem-core/src/main/java/com/griddynamics/gemfire/serialization/codegen/impl/primitive/FromDataPrimitiveProcessor.java
@@ -0,0 +1,24 @@
+package com.griddynamics.gemfire.serialization.codegen.impl.primitive;
+
+import com.griddynamics.gemfire.serialization.codegen.XField;
+import com.griddynamics.gemfire.serialization.codegen.impl.FromDataProcessor;
+
+import static com.griddynamics.gemfire.serialization.codegen.CodeGenUtils.firstLetterToUpperCase;
+
+/**
+ * @author igolovach
+ */
+
+public class FromDataPrimitiveProcessor implements FromDataProcessor {
+    private final String dataInputMethod;
+
+    public FromDataPrimitiveProcessor(String dataInputMethod) {
+        this.dataInputMethod = dataInputMethod;
+    }
+
+    public String process(XField field) {
+        String fieldName = field.getName();
+
+        return "result.set" + firstLetterToUpperCase(fieldName) + "(in." + dataInputMethod + "());\n";
+    }
+}
