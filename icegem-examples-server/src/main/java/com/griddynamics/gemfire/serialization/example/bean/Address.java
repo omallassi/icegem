32617cf34646bbe07104348950fc5f4468de1bd4
YuliyaAidarova <YuliyaAidarova@gmail.com>
2011-02-25 14:44:11 +0000

    enchancement of pom files and adding checking of parent classes

---
+++ icegem-examples-server/src/main/java/com/griddynamics/gemfire/serialization/example/bean/Address.java
@@ -0,0 +1,38 @@
+package com.griddynamics.gemfire.serialization.example.bean;
+
+import com.griddynamics.gemfire.serialization.SerializedClass;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+/**
+ * @author igolovach
+ */
+@SerializedClass(dataSerializerID = 1)
+public class Address extends ParentAddress {
+    private String street;
+    private String zipCode;
+
+    public Address() {
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
+    }
+}
---
+++ icegem-examples-server/src/main/java/com/griddynamics/gemfire/serialization/example/bean/ParentParentCompany.java
@@ -0,0 +1,18 @@
+package com.griddynamics.gemfire.serialization.example.bean;
+
+/**
+ * @author igolovach
+ */
+
+public class ParentParentCompany {
+
+    private int parentParentData;
+
+    public int getParentParentData() {
+        return parentParentData;
+    }
+
+    public void setParentParentData(int someParentData) {
+        this.parentParentData = someParentData;
+    }
+}
