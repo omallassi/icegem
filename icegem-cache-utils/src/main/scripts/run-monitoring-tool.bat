33eacb4cefc1b37f95e144933854560f192886f3
denis.box <denis.box@gmail.com>
2011-05-11 09:31:58 +0000

    Monitoring tool refactoring, adding unit tests and javadocs.

--- icegem-cache-utils/src/main/scripts/run-monitoring-tool.bat
+++ icegem-cache-utils/src/main/scripts/run-monitoring-tool.bat
@@ -1,2 +1,2 @@
-set SERVER_CLASSPATH=.;lib\gemfire-6.5.1.2.jar;lib\mail-1.4.1.jar;lib\activation-1.1.jar
+set SERVER_CLASSPATH=.;lib\gemfire-${com.gemstone.gemfire.version}.jar;lib\mail-1.4.1.jar;lib\activation-1.1.jar;lib\slf4j-api-${slf4j.api.version}.jar;lib\slf4j-log4j12-${slf4j.impl.version}.jar;lib\log4j-${log4j.version}.jar
 java -classpath %SERVER_CLASSPATH% com.googlecode.icegem.cacheutils.monitor.MonitoringTool
--- icegem-cache-utils/src/main/scripts/stop-locator.bat
+++ icegem-cache-utils/src/main/scripts/stop-locator.bat
@@ -1,1 +1,1 @@
-java -cp lib\gemfire-6.5.1.2.jar com.gemstone.gemfire.internal.SystemAdmin stop-locator -port=10355 -Dgemfire.mcast-port=0
+java -cp lib\gemfire-${com.gemstone.gemfire.version}.jar com.gemstone.gemfire.internal.SystemAdmin stop-locator -port=10355 -Dgemfire.mcast-port=0
