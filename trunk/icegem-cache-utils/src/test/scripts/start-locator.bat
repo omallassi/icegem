4ed32ecb32e0ac42aed6538b86219dd57aac8bba
denis.box@gmail.com <denis.box@gmail.com@d3097f50-16a0-dd82-721a-b39e0efbcae2>
2011-05-13 08:47:49 +0000

    The integration test does not depend from the version of the installed GemFire now

--- trunk/icegem-cache-utils/src/test/scripts/start-locator.bat
+++ trunk/icegem-cache-utils/src/test/scripts/start-locator.bat
@@ -1,1 +1,1 @@
-%GEMFIRE%\bin\gemfire start-locator -port=10355 -Dgemfire.mcast-port=0 -dir=locator
+java -cp ${gemfire.jar} com.gemstone.gemfire.internal.SystemAdmin start-locator -port=10355 -Dgemfire.mcast-port=0 -dir=locator
