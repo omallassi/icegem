ef8d57e753d010f2ca5584bef767bef670a8e007
stepanov.standy@gmail.com <stepanov.standy@gmail.com@d3097f50-16a0-dd82-721a-b39e0efbcae2>
2011-05-17 15:00:10 +0000

    Common system for managing integration tests has been added into core and cache-utils modules.

--- trunk/icegem-core/src/test/java/itest/com/googlecode/icegem/expiration/ExpirationControllerTest.java
+++ trunk/icegem-core/src/test/java/itest/com/googlecode/icegem/expiration/ExpirationControllerTest.java
@@ -43,231 +43,227 @@ import com.googlecode.icegem.utils.JavaProcessLauncher;
  */
 public class ExpirationControllerTest implements Serializable {
 
-	private static final long serialVersionUID = -1467927314327799826L;
+    private static final long serialVersionUID = -1467927314327799826L;
 
-	private static final long EXPIRATION_TIME = 1 * 1000;
+    private static final long EXPIRATION_TIME = 1 * 1000;
 
-	/** Field cacheServer1 */
-	private static Process cacheServer1;
-	/** Field cacheServer2 */
-	private static Process cacheServer2;
-	/** Field javaProcessLauncher */
-	private static JavaProcessLauncher javaProcessLauncher = new JavaProcessLauncher();
+    /** Field cacheServer1 */
+    private static Process cacheServer1;
+    /** Field cacheServer2 */
+    private static Process cacheServer2;
+    /** Field javaProcessLauncher */
+    private static JavaProcessLauncher javaProcessLauncher = new JavaProcessLauncher();
 
-	private class TransactionExpirationPolicy implements ExpirationPolicy {
+    private class TransactionExpirationPolicy implements ExpirationPolicy {
 
-		private static final long serialVersionUID = -8642198262421835809L;
+        private static final long serialVersionUID = -8642198262421835809L;
 
-		private long expirationTime;
-		private boolean recursively;
+        private long expirationTime;
+        private boolean recursively;
 
-		public TransactionExpirationPolicy(long expirationTime,
-			boolean recursively) {
-			this.expirationTime = expirationTime;
-			this.recursively = recursively;
-		}
+        public TransactionExpirationPolicy(long expirationTime,
+                                           boolean recursively) {
+            this.expirationTime = expirationTime;
+            this.recursively = recursively;
+        }
 
-		private boolean isTimeExpired(long finishedAt) {
-			boolean timeExpired = false;
+        private boolean isTimeExpired(long finishedAt) {
+            boolean timeExpired = false;
 
-			long checkedAt = System.currentTimeMillis();
-			long idleDuration = checkedAt - finishedAt;
+            long checkedAt = System.currentTimeMillis();
+            long idleDuration = checkedAt - finishedAt;
 
-			if (idleDuration > expirationTime) {
-				timeExpired = true;
-			}
+            if (idleDuration > expirationTime) {
+                timeExpired = true;
+            }
 
-			return timeExpired;
-		}
+            return timeExpired;
+        }
 
-		public boolean isExpired(Entry<Object, Object> entry) {
+        public boolean isExpired(Entry<Object, Object> entry) {
 
-			boolean expired = false;
+            boolean expired = false;
 
-			Object key = entry.getKey();
-			Object value = entry.getValue();
+            Object key = entry.getKey();
+            Object value = entry.getValue();
 
-			if ((key instanceof Long) && (value instanceof Transaction)) {
-				Long transactionId = (Long) key;
-				Transaction transaction = (Transaction) value;
+            if ((key instanceof Long) && (value instanceof Transaction)) {
+                Long transactionId = (Long) key;
+                Transaction transaction = (Transaction) value;
 
-				boolean timeExpired = isTimeExpired(transaction.getFinishedAt());
+                boolean timeExpired = isTimeExpired(transaction.getFinishedAt());
 
-				RegionService regionService = entry.getRegion()
-					.getRegionService();
-				Region<Long, TransactionProcessingError> errorsRegion = regionService
-					.getRegion("errors");
+                RegionService regionService = entry.getRegion()
+                        .getRegionService();
+                Region<Long, TransactionProcessingError> errorsRegion = regionService
+                        .getRegion("errors");
 
-				if (transaction.isProcessedSuccessfully() && timeExpired) {
-					expired = true;
-				} else {
-					TransactionProcessingError error = errorsRegion
-						.get(transactionId);
+                if (transaction.isProcessedSuccessfully() && timeExpired) {
+                    expired = true;
+                } else {
+                    TransactionProcessingError error = errorsRegion
+                            .get(transactionId);
 
-					if (error != null) {
-						timeExpired = isTimeExpired(error.getResolvedAt());
-						if (error.isResolved() && timeExpired) {
-							expired = true;
-						}
-					}
-				}
+                    if (error != null) {
+                        timeExpired = isTimeExpired(error.getResolvedAt());
+                        if (error.isResolved() && timeExpired) {
+                            expired = true;
+                        }
+                    }
+                }
 
-				if (expired && recursively) {
-					TransactionProcessingError error = errorsRegion
-						.get(transactionId);
-					if (error != null) {
-						errorsRegion.destroy(transactionId);
-					}
-				}
+                if (expired && recursively) {
+                    TransactionProcessingError error = errorsRegion
+                            .get(transactionId);
+                    if (error != null) {
+                        errorsRegion.destroy(transactionId);
+                    }
+                }
 
-			}
+            }
 
-			return expired;
-		}
+            return expired;
+        }
 
-	}
+    }
 
-	@BeforeClass(enabled = true)
-	public void setUp() throws IOException, InterruptedException,
-		TimeoutException {
-		startCacheServers();
-	}
+    @BeforeClass(enabled = true)
+    public void setUp() throws IOException, InterruptedException,
+            TimeoutException {
+        startCacheServers();
+    }
 
-	@AfterClass(enabled = true)
-	public void tearDown() throws IOException, InterruptedException {
-		stopCacheServers();
-	}
+    @AfterClass(enabled = true)
+    public void tearDown() throws IOException, InterruptedException {
+        stopCacheServers();
+    }
 
-	@Test(enabled = true)
-	public void testNotRecursiveProcess() throws Throwable {
-		fillData();
-		assertThat(5, 2);
-		Thread.sleep(3 * 1000);
-		long destroyedEntriesNumber = expire(false);
-		Assertions.assertThat(destroyedEntriesNumber).isEqualTo(2);
-		assertThat(3, 2);
-	}
+    @Test(enabled = true)
+    public void testNotRecursiveProcess() throws Throwable {
+        fillData();
+        assertThat(5, 2);
+        Thread.sleep(3 * 1000);
+        long destroyedEntriesNumber = expire(false);
+        Assertions.assertThat(destroyedEntriesNumber).isEqualTo(2);
+        assertThat(3, 2);
+    }
 
-	@Test(enabled = true)
-	public void testRecursiveProcess() throws InterruptedException {
-		fillData();
-		assertThat(5, 2);
-		Thread.sleep(3 * 1000);
+    @Test(enabled = true)
+    public void testRecursiveProcess() throws InterruptedException {
+        fillData();
+        assertThat(5, 2);
+        Thread.sleep(3 * 1000);
 
-		long destroyedEntriesNumber = expire(true);
-		Assertions.assertThat(destroyedEntriesNumber).isEqualTo(2);
-		assertThat(3, 1);
-	}
+        long destroyedEntriesNumber = expire(true);
+        Assertions.assertThat(destroyedEntriesNumber).isEqualTo(2);
+        assertThat(3, 1);
+    }
 
-	private <K, V> Region<K, V> getRegion(ClientCache cache, String regionName) {
-		ClientRegionFactory<K, V> clientRegionFactory = cache
-			.createClientRegionFactory(ClientRegionShortcut.PROXY);
+    private <K, V> Region<K, V> getRegion(ClientCache cache, String regionName) {
+        ClientRegionFactory<K, V> clientRegionFactory = cache
+                .createClientRegionFactory(ClientRegionShortcut.PROXY);
 
-		Region<K, V> region = cache.getRegion(regionName);
+        Region<K, V> region = cache.getRegion(regionName);
 
-		if (region == null) {
-			region = clientRegionFactory.create(regionName);
-		}
+        if (region == null) {
+            region = clientRegionFactory.create(regionName);
+        }
 
-		return region;
-	}
+        return region;
+    }
 
-	private void fillData() {
-		ClientCache cache = new ClientCacheFactory()
-			.addPoolLocator("localhost", 10355).set("log-level", "warning")
-			.create();
+    private void fillData() {
+        ClientCache cache = new ClientCacheFactory()
+                .addPoolLocator("localhost", 10355).set("log-level", "warning")
+                .create();
 
-		Region<Long, Transaction> transactionsRegion = getRegion(cache,
-			"transactions");
-		CacheUtils.clearPartitionedRegion(transactionsRegion);
+        Region<Long, Transaction> transactionsRegion = getRegion(cache,
+                "transactions");
+        CacheUtils.clearPartitionedRegion(transactionsRegion);
 
-		Transaction notStartedTransaction = new Transaction();
-		transactionsRegion.put(1L, notStartedTransaction);
+        Transaction notStartedTransaction = new Transaction();
+        transactionsRegion.put(1L, notStartedTransaction);
 
-		Transaction startedTransaction = new Transaction();
-		startedTransaction.begin();
-		transactionsRegion.put(2L, startedTransaction);
+        Transaction startedTransaction = new Transaction();
+        startedTransaction.begin();
+        transactionsRegion.put(2L, startedTransaction);
 
-		Transaction committedTransaction = new Transaction();
-		committedTransaction.begin();
-		committedTransaction.commit();
-		transactionsRegion.put(3L, committedTransaction);
+        Transaction committedTransaction = new Transaction();
+        committedTransaction.begin();
+        committedTransaction.commit();
+        transactionsRegion.put(3L, committedTransaction);
 
-		Transaction rolledbackUnresolvedTransaction = new Transaction();
-		rolledbackUnresolvedTransaction.begin();
-		rolledbackUnresolvedTransaction.rollback();
-		transactionsRegion.put(4L, rolledbackUnresolvedTransaction);
+        Transaction rolledbackUnresolvedTransaction = new Transaction();
+        rolledbackUnresolvedTransaction.begin();
+        rolledbackUnresolvedTransaction.rollback();
+        transactionsRegion.put(4L, rolledbackUnresolvedTransaction);
 
-		Transaction rolledbackResolvedTransaction = new Transaction();
-		rolledbackResolvedTransaction.begin();
-		rolledbackResolvedTransaction.rollback();
-		transactionsRegion.put(5L, rolledbackResolvedTransaction);
+        Transaction rolledbackResolvedTransaction = new Transaction();
+        rolledbackResolvedTransaction.begin();
+        rolledbackResolvedTransaction.rollback();
+        transactionsRegion.put(5L, rolledbackResolvedTransaction);
 
-		Region<Long, TransactionProcessingError> errorsRegion = getRegion(
-			cache, "errors");
-		CacheUtils.clearPartitionedRegion(errorsRegion);
+        Region<Long, TransactionProcessingError> errorsRegion = getRegion(
+                cache, "errors");
+        CacheUtils.clearPartitionedRegion(errorsRegion);
 
-		TransactionProcessingError unresolvedError = new TransactionProcessingError(
-			"Error during the transaction processing");
-		errorsRegion.put(4L, unresolvedError);
+        TransactionProcessingError unresolvedError = new TransactionProcessingError(
+                "Error during the transaction processing");
+        errorsRegion.put(4L, unresolvedError);
 
-		TransactionProcessingError resolvedError = new TransactionProcessingError(
-			"Error during the transaction processing");
-		resolvedError.setResolved();
-		errorsRegion.put(5L, resolvedError);
+        TransactionProcessingError resolvedError = new TransactionProcessingError(
+                "Error during the transaction processing");
+        resolvedError.setResolved();
+        errorsRegion.put(5L, resolvedError);
 
-		cache.close();
-	}
+        cache.close();
+    }
 
-	private void assertThat(int transactionsNumber, int errorsNumber) {
-		ClientCache cache = new ClientCacheFactory()
-			.addPoolLocator("localhost", 10355).set("log-level", "warning")
-			.create();
+    private void assertThat(int transactionsNumber, int errorsNumber) {
+        ClientCache cache = new ClientCacheFactory()
+                .addPoolLocator("localhost", 10355).set("log-level", "warning")
+                .create();
 
-		Region<Long, Transaction> transactionsRegion = getRegion(cache,
-			"transactions");
+        Region<Long, Transaction> transactionsRegion = getRegion(cache,
+                "transactions");
 
-		Region<Long, TransactionProcessingError> errorsRegion = getRegion(
-			cache, "errors");
+        Region<Long, TransactionProcessingError> errorsRegion = getRegion(
+                cache, "errors");
 
-		Assertions.assertThat(transactionsRegion.keySetOnServer().size())
-			.isEqualTo(transactionsNumber);
-		Assertions.assertThat(errorsRegion.keySetOnServer().size()).isEqualTo(
-			errorsNumber);
+        Assertions.assertThat(transactionsRegion.keySetOnServer().size())
+                .isEqualTo(transactionsNumber);
+        Assertions.assertThat(errorsRegion.keySetOnServer().size()).isEqualTo(
+                errorsNumber);
 
-		cache.close();
-	}
+        cache.close();
+    }
 
-	private long expire(boolean recursively) {
-		ExpirationController expirationController = new ExpirationController(
-			"127.0.0.1", 10355);
+    private long expire(boolean recursively) {
+        ExpirationController expirationController = new ExpirationController(
+                "127.0.0.1", 10355);
 
-		long destroyedEntriesNumber = expirationController.process(
-			"transactions", new TransactionExpirationPolicy(EXPIRATION_TIME,
-				recursively));
+        long destroyedEntriesNumber = expirationController.process(
+                "transactions", new TransactionExpirationPolicy(EXPIRATION_TIME,
+                        recursively));
 
-		expirationController.close();
+        expirationController.close();
 
-		return destroyedEntriesNumber;
+        return destroyedEntriesNumber;
 
-	}
+    }
 
-	private void startCacheServers() throws IOException, InterruptedException {
-		cacheServer1 = javaProcessLauncher.runWithConfirmation(Server.class,
-			new String[] { CacheUtils.START_EMBEDDED_LOCATOR_COMMAND });
-		cacheServer2 = javaProcessLauncher.runWithConfirmation(Server.class);
-	}
+    private void startCacheServers() throws IOException, InterruptedException {
+        cacheServer1 = javaProcessLauncher.runWithConfirmation(Server.class);
+        cacheServer2 = javaProcessLauncher.runWithConfirmation(Server.class);
+    }
 
-	private void stopServer(Process server) throws IOException,
-		InterruptedException {
-		javaProcessLauncher.stopBySendingNewLineIntoProcess(server);
+    private void stopServer(Process server) throws IOException,
+            InterruptedException {
+        javaProcessLauncher.stopBySendingNewLineIntoProcess(server);
+    }
 
-		server.waitFor();
-	}
-
-	private void stopCacheServers() throws IOException, InterruptedException {
-		stopServer(cacheServer1);
-		stopServer(cacheServer2);
-	}
-
+    private void stopCacheServers() throws IOException, InterruptedException {
+        stopServer(cacheServer1);
+        stopServer(cacheServer2);
+    }
 }
