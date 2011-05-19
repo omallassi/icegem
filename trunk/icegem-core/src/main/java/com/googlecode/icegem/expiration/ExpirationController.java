eabc800c7e5b4f1dc057f508c25130a58cb9da66
denis.box@gmail.com <denis.box@gmail.com@d3097f50-16a0-dd82-721a-b39e0efbcae2>
2011-05-19 11:24:14 +0000

    Logging in ExpirationController and ExpirationFunction has been added. Also the Spring wrapper ExpirationControllerSpringWrapper has been added.

--- trunk/icegem-core/src/main/java/com/googlecode/icegem/expiration/ExpirationController.java
+++ trunk/icegem-core/src/main/java/com/googlecode/icegem/expiration/ExpirationController.java
@@ -1,5 +1,8 @@
 package com.googlecode.icegem.expiration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.gemstone.gemfire.cache.Region;
 import com.gemstone.gemfire.cache.client.ClientCache;
 import com.gemstone.gemfire.cache.client.ClientCacheFactory;
@@ -67,8 +70,33 @@ import com.gemstone.gemfire.cache.execute.FunctionService;
  */
 public class ExpirationController {
 
+	private Logger logger = LoggerFactory.getLogger(ExpirationController.class);
+
 	private ClientCache cache;
 	private ClientRegionFactory<Object, Object> clientRegionFactory;
+
+	/**
+	 * Creates the instance of ExpirationController using external client cache
+	 * and region.
+	 * 
+	 * Good to use with Spring.
+	 * 
+	 * @param cache
+	 *            - the client cache configured outside
+	 * @param clientRegionFactory
+	 *            - the client region factory configured outside
+	 */
+	public ExpirationController(ClientCache cache,
+		ClientRegionFactory<Object, Object> clientRegionFactory) {
+		logger.info("Creating the ExpirationController");
+
+		this.cache = cache;
+		this.clientRegionFactory = clientRegionFactory;
+
+		logger.info("ExpirationController with parameters cache = " + cache
+			+ ", clientRegionFactory = " + clientRegionFactory
+			+ " has been created");
+	}
 
 	/**
 	 * Creates the instance of ExpirationController for the specified locator.
@@ -79,22 +107,34 @@ public class ExpirationController {
 	 *            - the port of locator
 	 */
 	public ExpirationController(String locatorHost, int locatorPort) {
-		if ((locatorHost == null) || (locatorHost.trim().length() == 0)) {
-			throw new IllegalArgumentException("The locator host \""
-				+ locatorHost + "\" is incorrect");
+		logger.info("Creating the ExpirationController");
+
+		try {
+			if ((locatorHost == null) || (locatorHost.trim().length() == 0)) {
+				throw new IllegalArgumentException("The locator host \""
+					+ locatorHost + "\" is incorrect");
+			}
+
+			if ((locatorPort < 0) || (locatorPort > 65535)) {
+				throw new IllegalArgumentException("The locator port \""
+					+ locatorPort
+					+ "\" is incorrect. It should be in range [0, 65535]");
+			}
+
+			cache = new ClientCacheFactory().set("log-level", "warning")
+				.addPoolLocator(locatorHost, locatorPort).create();
+
+			clientRegionFactory = cache
+				.createClientRegionFactory(ClientRegionShortcut.PROXY);
+		} catch (RuntimeException re) {
+			logger.error(
+				"RuntimeException during creating of ExpirationController", re);
+			throw re;
 		}
 
-		if ((locatorPort < 0) || (locatorPort > 65535)) {
-			throw new IllegalArgumentException("The locator port \""
-				+ locatorPort
-				+ "\" is incorrect. It should be in range [0, 65535].");
-		}
-
-		cache = new ClientCacheFactory().set("log-level", "warning")
-			.addPoolLocator(locatorHost, locatorPort).create();
-
-		clientRegionFactory = cache
-			.createClientRegionFactory(ClientRegionShortcut.PROXY);
+		logger.info("ExpirationController with parameters locatorHost = "
+			+ locatorHost + ", locatorPort = " + locatorPort
+			+ " has been created");
 	}
 
 	/**
@@ -114,44 +154,72 @@ public class ExpirationController {
 	 */
 	public long process(String regionName, ExpirationPolicy policy,
 		long packetSize, long packetDelay) {
-		if (cache == null) {
-			throw new IllegalStateException(
-				"It seems that the workflow of the controller is already finished");
-		}
-
-		if ((regionName == null) || (regionName.trim().length() == 0)) {
-			throw new IllegalArgumentException("The region name \""
-				+ regionName + "\" is incorrect");
-		}
-
-		if (policy == null) {
-			throw new IllegalArgumentException(
-				"The ExpirationPolicy cannot be null");
-		}
-
-		Region<Object, Object> region = cache.getRegion(regionName);
-
-		if (region == null) {
-			region = clientRegionFactory.create(regionName);
-		}
-
-		if (region == null) {
-			throw new IllegalStateException(
-				"Cannot retrieve access to the region with name \""
-					+ regionName + "\"");
-		}
 
 		long destroyedEntriesNumber = 0;
 
-		DestroyedEntriesCountCollector collector = (DestroyedEntriesCountCollector) FunctionService
-			.onRegion(region)
-			.withArgs(new ExpirationFunctionArguments(packetSize, packetDelay))
-			.withCollector(new DestroyedEntriesCountCollector())
-			.execute(new ExpirationFunction(policy));
+		try {
 
-		Object result = collector.getResult();
-		if (result instanceof Long) {
-			destroyedEntriesNumber = (Long) result;
+			if (cache == null) {
+				throw new IllegalStateException(
+					"It seems that the workflow of the controller is already finished");
+			}
+
+			if ((regionName == null) || (regionName.trim().length() == 0)) {
+				throw new IllegalArgumentException("The region name \""
+					+ regionName + "\" is incorrect");
+			}
+
+			if (policy == null) {
+				throw new IllegalArgumentException(
+					"The ExpirationPolicy cannot be null");
+			}
+
+			Region<Object, Object> region = cache.getRegion(regionName);
+
+			if (region == null) {
+				region = clientRegionFactory.create(regionName);
+			}
+
+			if (region == null) {
+				throw new IllegalStateException(
+					"Cannot retrieve access to the region with name \""
+						+ regionName + "\"");
+			}
+
+			logger
+				.info("Running ExpirationController process with parameters regionName = "
+					+ regionName
+					+ ", policy = "
+					+ policy
+					+ ", packetSize = "
+					+ packetSize + ", packetDelay = " + packetDelay);
+
+			DestroyedEntriesCountCollector collector = (DestroyedEntriesCountCollector) FunctionService
+				.onRegion(region)
+				.withArgs(
+					new ExpirationFunctionArguments(packetSize, packetDelay))
+				.withCollector(new DestroyedEntriesCountCollector())
+				.execute(new ExpirationFunction(policy));
+
+			Object result = collector.getResult();
+			if (result instanceof Long) {
+				destroyedEntriesNumber = (Long) result;
+			}
+
+			logger
+				.info("ExpirationController process with parameters regionName = "
+					+ regionName
+					+ ", policy = "
+					+ policy
+					+ ", packetSize = "
+					+ packetSize
+					+ ", packetDelay = "
+					+ packetDelay
+					+ " has destroyed " + destroyedEntriesNumber + " entries");
+
+		} catch (RuntimeException re) {
+			logger.error("RuntimeException during processing", re);
+			throw re;
 		}
 
 		return destroyedEntriesNumber;
@@ -177,8 +245,10 @@ public class ExpirationController {
 	 * Finishes the workflow of controller.
 	 */
 	public void close() {
+		logger.info("Closing the ExpirationController");
 		if (cache != null) {
 			cache.close();
 		}
+		logger.info("The ExpirationController has been closed");
 	}
 }
