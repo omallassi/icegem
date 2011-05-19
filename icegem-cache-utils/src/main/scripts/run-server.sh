3bcffbaed2143bacd61323876042f1b4dfef08fe
denis.box <denis.box@gmail.com>
2011-05-19 15:06:14 +0000

    Assembly configuration for icegem-cache-utils has been updated

--- icegem-cache-utils/src/main/scripts/run-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-set SERVER_CLASSPATH=.:lib/gemfire-${com.gemstone.gemfire.version}.jar:lib/icegem-core-${version}.jar
-java -DgemfirePropertyFile=gemfire-monitor.properties -classpath $SERVER_CLASSPATH com.googlecode.icegem.cacheutils.monitor.server.Server
--- icegem-cache-utils/src/main/scripts/start-locator.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -cp lib/gemfire-${com.gemstone.gemfire.version}.jar com.gemstone.gemfire.internal.SystemAdmin start-locator -port=10355 -Dgemfire.mcast-port=0
