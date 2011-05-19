4252ef8a049087bc8d2127394b60c390c9c209cf
denis.box@gmail.com <denis.box@gmail.com@d3097f50-16a0-dd82-721a-b39e0efbcae2>
2011-05-19 15:06:14 +0000

    Assembly configuration for icegem-cache-utils has been updated

--- trunk/icegem-cache-utils/src/main/scripts/run-monitoring-tool.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-set SERVER_CLASSPATH=.:lib/gemfire-${com.gemstone.gemfire.version}.jar:lib/mail-1.4.1.jar:lib/activation-1.1.jar:lib/log4j-${log4j.version}.jar
-java -classpath $SERVER_CLASSPATH com.googlecode.icegem.cacheutils.monitor.MonitoringTool
--- trunk/icegem-cache-utils/src/main/scripts/run-server.bat
+++
@@ -1,2 +0,0 @@
-set SERVER_CLASSPATH=.;lib\gemfire-${com.gemstone.gemfire.version}.jar;lib\icegem-core-${version}.jar
-java -DgemfirePropertyFile=gemfire-monitor.properties -classpath %SERVER_CLASSPATH% com.googlecode.icegem.cacheutils.monitor.server.Server
