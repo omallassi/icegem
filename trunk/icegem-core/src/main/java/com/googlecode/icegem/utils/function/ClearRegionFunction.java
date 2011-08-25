1fa8cd4dfd3d3b795766b6add427f36792aaa44e
renat.akhmerov@gmail.com <renat.akhmerov@gmail.com@d3097f50-16a0-dd82-721a-b39e0efbcae2>
2011-08-25 15:40:11 +0000

    1. Adding first version of abstract integration test. 2. Cosmetic changes.

--- trunk/icegem-core/src/main/java/com/googlecode/icegem/utils/function/ClearRegionFunction.java
+++ trunk/icegem-core/src/main/java/com/googlecode/icegem/utils/function/ClearRegionFunction.java
@@ -19,38 +19,43 @@ import java.util.Set;
 public class ClearRegionFunction extends FunctionAdapter {
     private final static String FUNCTION_ID = ClearRegionFunction.class.getName();
 
-    @SuppressWarnings({"ThrowableInstanceNeverThrown"})
+    @SuppressWarnings({ "ThrowableInstanceNeverThrown" })
     @Override
     public void execute(FunctionContext functionContext) {
-        ResultSender<Boolean> rs = functionContext.getResultSender();
-        Object arg = functionContext.getArguments();
+	ResultSender<Boolean> rs = functionContext.getResultSender();
+	Object arg = functionContext.getArguments();
 
-        if (!(arg instanceof String)) {
-            rs.sendException(new IllegalStateException("Function parameter must be instance of String.class"));
-            return;
-        }
+	if (!(arg instanceof String)) {
+	    rs.sendException(new IllegalStateException("Function parameter must be instance of String.class"));
+	    
+	    return;
+	}
 
-        String regionName = (String) arg;
+	String regionName = (String) arg;
 
-        Region<Object, Object> region = CacheFactory.getAnyInstance().getRegion(regionName);
-        if (region == null) {
-            rs.sendException(new RegionNotFoundException("Region '" + regionName + "' does not exist on this member"));
-            return;
-        }
+	Region<Object, Object> region = CacheFactory.getAnyInstance().getRegion(regionName);
+	
+	if (region == null) {
+	    rs.sendException(new RegionNotFoundException("Region '" + regionName + "' does not exist on this member"));
+	    
+	    return;
+	}
 
-        if (region.getAttributes().getDataPolicy().withPartitioning()) {
-            Set<Object> keys = PartitionRegionHelper.getLocalPrimaryData(region).keySet();
-            for (Object key : keys) {
-                region.destroy(key);
-            }
-        } else {
-            region.clear();
-        }
-        rs.lastResult(true);
+	if (region.getAttributes().getDataPolicy().withPartitioning()) {
+	    Set<Object> keys = PartitionRegionHelper.getLocalPrimaryData(region).keySet();
+	    
+	    for (Object key : keys) {
+		region.destroy(key);
+	    }
+	} else {
+	    region.clear();
+	}
+	
+	rs.lastResult(true);
     }
 
     @Override
     public String getId() {
-        return FUNCTION_ID;
+	return FUNCTION_ID;
     }
 }
