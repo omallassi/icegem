f77d0476b1da42cf844f8a062e347c0b9c6425e8
renat.akhmerov <renat.akhmerov@gmail.com>
2011-08-25 15:40:11 +0000

    1. Adding first version of abstract integration test. 2. Cosmetic changes.

--- icegem-core/src/test/java/itest/com/googlecode/icegem/utils/CacheUtilsTest.java
+++ icegem-core/src/test/java/itest/com/googlecode/icegem/utils/CacheUtilsTest.java
@@ -1,40 +1,25 @@
 package itest.com.googlecode.icegem.utils;
 
-import static junit.framework.Assert.*;
+import static com.gemstone.gemfire.cache.client.ClientRegionShortcut.PROXY;
+import static junit.framework.Assert.assertEquals;
+import itest.com.googlecode.icegem.AbstractIntegrationTest;
 
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
+import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.gemstone.gemfire.cache.Region;
-import com.gemstone.gemfire.cache.client.ClientCache;
-import com.gemstone.gemfire.cache.client.ClientCacheFactory;
-import com.gemstone.gemfire.cache.client.ClientRegionFactory;
-import com.gemstone.gemfire.cache.client.ClientRegionShortcut;
 import com.googlecode.icegem.utils.CacheUtils;
-import com.googlecode.icegem.utils.JavaProcessLauncher;
-import com.googlecode.icegem.utils.PropertiesHelper;
-import com.googlecode.icegem.utils.ServerTemplate;
 
 /**
- * TODO: need to implement abstract integration test in order to avoid
- * code duplicates all over the place in the tests.
- * 
  * Tests for {@link CacheUtils} class. 
  */
-public class CacheUtilsTest {
-    /** */
+public class CacheUtilsTest extends AbstractIntegrationTest {
+    /** GemFire property file name. */
     private static final String PROP_FILE = "cacheUtilsTestServerProperties.properties";
-
-    /** Locator port. */
-    private static final int LOCATOR_PORT = 10355;
-
-    /** Cache. */
-    private static ClientCache cache;
 
     /** Partitioned region. */
     private static Region<Object, Object> partitionedRegion;
@@ -42,82 +27,60 @@ public class CacheUtilsTest {
     /** Replicated region. */
     private static Region<Object, Object> replicatedRegion;
 
-    /** Cache server 1. */
-    private static Process cacheServer1;
-
-    /** Cache server 2. */
-    private static Process cacheServer2;
-
-    /** Java process launcher. */
-    private static JavaProcessLauncher javaProcessLauncher = new JavaProcessLauncher();
-
     @BeforeClass
-    public static void setUp() throws IOException, InterruptedException, TimeoutException {
-	startCacheServers();
-
-	startClient();
-
-	CacheUtils.clearRegion(partitionedRegion);
+    public static void beforeAllTests() throws Exception {
+	startCacheServersAndClient(2, PROP_FILE);
+	
+	partitionedRegion = createClientRegion("partitioned_region", PROXY);
+	replicatedRegion = createClientRegion("replicated_region", PROXY);
     }
 
     @AfterClass
-    public static void tearDown() throws IOException, InterruptedException {
-	cache.close();
-
-	stopCacheServers();
+    public static void afterAllTests() throws Exception {
+	stopCacheServersAndClient();
     }
 
-    @Before
-    public void after() throws InterruptedException, IOException {
-	CacheUtils.clearRegion(partitionedRegion);
-	CacheUtils.clearRegion(replicatedRegion);
+    @After
+    public void afterTest() throws InterruptedException, IOException {
+	clearClientRegions();
     }
 
     /**
-     * Starts a client.
-     * 
-     * @throws java.io.IOException
+     * JUnit.
      */
-    private static void startClient() throws IOException {
-	ClientCacheFactory clientCacheFactory = new ClientCacheFactory().addPoolLocator("localhost", LOCATOR_PORT);
+    @Test
+    public void testPartitionedRegionSize() {
+	assertEquals(0, CacheUtils.getRegionSize(partitionedRegion));
 
-	PropertiesHelper properties = new PropertiesHelper("/" + PROP_FILE);
+	int keyCnt = 100;
 
-	cache = clientCacheFactory.set("log-level", properties.getStringProperty("log-level"))
-		.set("license-file", properties.getStringProperty("license-file"))
-		.set("license-type", properties.getStringProperty("license-type")).create();
+	for (int i = 0; i < keyCnt; i++) {
+	    partitionedRegion.put(i, "Value" + i);
 
-	ClientRegionFactory<Object, Object> regionFactory = cache.createClientRegionFactory(ClientRegionShortcut.PROXY);
+	    if (i % 50 == 0)
+		info("Stored key-value pairs: " + i);
+	}
 
-	partitionedRegion = regionFactory.create("partitioned_region");
-	replicatedRegion = regionFactory.create("replicated_region");
+	assertEquals(keyCnt, CacheUtils.getRegionSize(partitionedRegion));
     }
 
     /**
-     * Starts two cache servers for tests.
-     * 
-     * @throws IOException
-     *             when
-     * @throws InterruptedException
-     *             when
+     * JUnit.
      */
-    private static void startCacheServers() throws IOException, InterruptedException {
-	String[] javaArgs = new String[] { "-DgemfirePropertyFile=" + PROP_FILE };
+    @Test
+    public void testReplicatedRegionSize() {
+	assertEquals(0, CacheUtils.getRegionSize(replicatedRegion));
 
-	cacheServer1 = javaProcessLauncher.runWithConfirmation(ServerTemplate.class, javaArgs, null);
-	cacheServer2 = javaProcessLauncher.runWithConfirmation(ServerTemplate.class, javaArgs, null);
-    }
+	int keyCnt = 100;
 
-    /**
-     * Stops cache servers.
-     * 
-     * @throws IOException
-     *             when
-     * @throws InterruptedException
-     */
-    private static void stopCacheServers() throws IOException, InterruptedException {
-	javaProcessLauncher.stopBySendingNewLineIntoProcess(cacheServer1);
-	javaProcessLauncher.stopBySendingNewLineIntoProcess(cacheServer2);
+	for (int i = 0; i < keyCnt; i++) {
+	    replicatedRegion.put(i, "Value" + i);
+
+	    if (i % 50 == 0)
+		info("Stored key-value pairs: " + i);
+	}
+
+	assertEquals(keyCnt, CacheUtils.getRegionSize(replicatedRegion));
     }
 
     /**
@@ -135,38 +98,8 @@ public class CacheUtilsTest {
      * JUnit.
      */
     @Test
-    public void testPartitionedRegionSize() {
-	assertEquals(0, CacheUtils.getRegionSize(partitionedRegion));
-	
-	int keyCnt = 1000;
-	
-	for (int i = 0; i < keyCnt; i++)
-	    partitionedRegion.put(i, "Value" + i);
-	
-	assertEquals(keyCnt, CacheUtils.getRegionSize(partitionedRegion));
-    }
-    
-    /**
-     * JUnit.
-     */
-    @Test
-    public void testReplicatedRegionSize() {
-	assertEquals(0, CacheUtils.getRegionSize(replicatedRegion));
-
-	int keyCnt = 1000;
-	
-	for (int i = 0; i < keyCnt; i++)
-	    replicatedRegion.put(i, "Value" + i);
-	
-	assertEquals(keyCnt, CacheUtils.getRegionSize(replicatedRegion));
-    }
-    
-    /**
-     * JUnit.
-     */
-    @Test
     public void testRetryWithExponentialBackoffNoRetry() {
-	//fail("Not yet implemented");
+	// fail("Not yet implemented");
     }
 
     /**
@@ -174,7 +107,7 @@ public class CacheUtilsTest {
      */
     @Test
     public void testRetryWithExponentialBackoffSingeRetry() {
-	//fail("Not yet implemented");
+	// fail("Not yet implemented");
     }
 
     /**
@@ -182,6 +115,6 @@ public class CacheUtilsTest {
      */
     @Test
     public void testRetryWithExponentialBackoffNoSuccess() {
-	//fail("Not yet implemented");
+	// fail("Not yet implemented");
     }
 }
